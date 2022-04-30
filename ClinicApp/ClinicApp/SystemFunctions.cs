2b2e3d7f392dd4ac80bb4cc177b0b1d96a219ca2
Danilo-Babic <daniloraska@gmail.com>
2022-04-30 11:26:19 +0200

    Delete examination
    Delete function(3.1 and 3.3)

---
+++ ClinicApp/ClinicApp/SystemFunctions.cs
@@ -0,0 +1,116 @@
+﻿using ClinicApp.Clinic;
+using ClinicApp.Users;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace ClinicApp
+{
+    public enum Roles
+    {
+        Nobody, Admin, Secretary, Doctor, Patient
+    };
+
+    public class SystemFunctions
+    {
+
+        // Dictionary of users created for faster and easier acces to information from the database
+        public static Dictionary<string, User> Users { get; set; } = new Dictionary<string, User>();
+        public static Dictionary<string, Doctor> Doctors { get; set; } = new Dictionary<string, Doctor>();
+        public static Dictionary<string, Patient> Patients { get; set; } = new Dictionary<string, Patient>();
+        public static Dictionary<string, HealthRecord> HealthRecords { get; set; } = new Dictionary<string, HealthRecord>();
+        public static Dictionary<int, Examination> AllExamtinations { get; set; } = new Dictionary<int, Examination>();
+        public static Dictionary<int, Examination> CurrentExamtinations { get; set; } = new Dictionary<int, Examination>();
+
+        // User file path may change in release mode, this is the file path in debug mode
+
+        public static string UsersFilePath =  "../../../Data/users.txt";
+        public static string ExaminationsFilePath = "../../../Data/examinations.txt";
+        public static string HealthRecordsFilePath = "../../../Data/health_records.txt";
+        public static string PatientRequestsFilePath = "../../../Data/patient_requests.txt";
+
+
+        // Loads the information from the database into objects and adds them to coresponding dictionaries
+        public static void LoadData()
+        {
+            using (StreamReader reader = new StreamReader(UsersFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    User user = ParseUser(line);
+                    Users.Add(user.UserName, user);
+                    if (user.Role == Roles.Doctor)
+                    {
+                        Doctors.Add(user.UserName, (Doctor)user);
+                    }
+                    else if (user.Role == Roles.Patient) {
+                        Patients.Add(user.UserName, (Patient)user);
+                    }
+                }
+            }
+            using (StreamReader reader = new StreamReader(ExaminationsFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Examination examination = ParseExamination(line);
+                    if (!AllExamtinations.TryAdd(examination.ID, examination)) {
+                        AllExamtinations[examination.ID] = examination;
+                    }
+                    if (examination.DateTime.AddMinutes(15) > DateTime.Now)
+                    {
+                        if (!CurrentExamtinations.TryAdd(examination.ID, examination)) {
+                            CurrentExamtinations[examination.ID] = examination;
+                        };
+                    }
+                    foreach (int ID in CurrentExamtinations.Keys) {
+                        Examination currentExamination = CurrentExamtinations[ID];
+                        if (currentExamination.Tombstone)
+                        {
+                            CurrentExamtinations.Remove(ID);
+                        }
+                        else currentExamination.Doctor.Examinations.Add(currentExamination);
+                    }
+                }
+            }
+
+        }
+
+        private static Examination ParseExamination(string line)
+        {
+            return new Examination(line);
+        }
+
+        // Parsing functions
+        private static User ParseUser(string line)
+        {
+            string[] data = line.Split('|');
+            if(data[6] == Roles.Admin.ToString())
+                return new Users.Admin(line);
+            if(data[6] == Roles.Secretary.ToString())
+                return new Secretary(line);
+            if(data[6] == Roles.Doctor.ToString())
+                return new Doctor(line);
+            if(data[6] == Roles.Patient.ToString())
+                return new Patient(line);
+            return new Nobody();
+        }
+
+        // Uploads the information from the objects into the database
+        public static void UploadData()
+        {
+            string newLine = "";
+            using (StreamWriter sw = File.CreateText(UsersFilePath))
+            {
+                foreach(KeyValuePair<string, User> pair in Users)
+                {
+                    newLine = pair.Value.Compress();
+                    sw.WriteLine(newLine);
+                }
+            }
+        }
+    }
+}
---
+++ ClinicApp/ClinicApp/Users/Patient.cs
@@ -0,0 +1,140 @@
+﻿using ClinicApp.Clinic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ClinicApp.Users
+{
+    public class Patient : User
+    {
+        public List<Examination> Examinations { get; set; }
+        //public Dictionary<>
+        //public Dictionary<DateTime, string>;
+
+        public Patient(string userName, string password, string name, string lastName, DateTime dateOfBirth, char gender)
+        {
+            UserName = userName;
+            Password = password;
+            Name = name;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Gender = gender;
+            Role = Roles.Patient;
+            Examinations = new List<Examination>();
+        }
+
+        public Patient(string text)
+        {
+            string[] data = text.Split("|");
+
+            UserName = data[0];
+            Password = data[1];
+            Name = data[2];
+            LastName = data[3];
+            DateOfBirth = DateTime.Parse(data[4]);
+            Gender = data[5][0];
+            Role = Roles.Patient;
+            Examinations = new List<Examination>();
+        }
+
+        public override string Compress()
+        {
+            return UserName + "|" + Password + "|" + Name + "|" + LastName + "|" + DateOfBirth.ToString("dd/MM/yyyy") + "|" + Gender + "|" + Role;
+        }
+
+        public override int MenuWrite()
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1: Log out");
+            Console.WriteLine("0: Exit");
+
+            return 1;
+        }
+
+        public override void MenuDo(int option)
+        {
+            switch (option)
+            {
+                case 2:
+                    //TODO
+                    break;
+            }
+        }
+
+        public static void ViewAllPatients()
+        {
+            int i = 1;
+            foreach (KeyValuePair<string, Patient> entry in SystemFunctions.Patients)
+            {
+                Patient patient = entry.Value;
+                Console.WriteLine($"{i}. User name: {patient.UserName}; Name: {patient.Name}; Last name: {patient.LastName}; Date of birth: {patient.DateOfBirth}");
+                i++;
+            }
+        }
+
+        public void InsertExamination(Examination newExamination)
+        {
+            if (this.Examinations.Count() == 0) {
+                this.Examinations.Add(newExamination);
+                return;
+            }
+            for (int i = 0; i < this.Examinations.Count(); i++)
+            {
+                if (this.Examinations[i].DateTime < newExamination.DateTime)
+                {
+                    Examinations.Insert(i, newExamination);
+                    return;
+                }
+            }
+            this.Examinations.Add(newExamination);
+
+        }
+
+        private void DeleteExamination()
+        {
+            Console.WriteLine("Enter the ID of the examination you want to delete?");
+            string id = Console.ReadLine();
+            Examination examination = null;
+            foreach (Examination tmp in this.Examinations)
+            {
+                if(tmp.ID == Convert.ToInt32(id))
+                {
+                    examination = tmp;
+                    DateTime now = DateTime.Now;
+                    Console.WriteLine("Are you sure? (y/n)");
+                    string choice = Console.ReadLine();
+                    int dateValidation = DateTime.Compare(now, tmp.DateTime);
+                    DateTime beforeExamination = tmp.DateTime - TimeSpan.FromDays(2);
+                    int dateValidationSecretary = DateTime.Compare(now, beforeExamination);
+                    if (choice.ToUpper() == "Y")
+                    {
+                        if (!(dateValidation<0))
+                        {
+                            Console.WriteLine("You can not perform this action.");
+                            return;
+                        }       
+                        else if (!(dateValidationSecretary<0))
+                        {
+                            Console.WriteLine("You can not perform this activity. Your request will be sent to secretary.");
+                            string line = examination.ID.ToString() + "|" + "DELETE";
+                            using (StreamWriter sw = File.AppendText(SystemFunctions.PatientRequestsFilePath))
+                            {
+                                sw.WriteLine(line);
+                            }
+                            //todo activity history write
+                            return;
+                        }
+                        this.Examinations.Remove(examination);
+                        Examination deletedExamination = new Examination(examination.ID, examination.DateTime,examination.Doctor,this,true);
+                        SystemFunctions.AllExamtinations.Add(deletedExamination.ID, deletedExamination);
+                        SystemFunctions.CurrentExamtinations.Remove(examination.ID);
+                        //todo remove from doctors schedule
+                        //todo acitvity history write
+                    }
+                }
+            }
+        }
+    }
+}
