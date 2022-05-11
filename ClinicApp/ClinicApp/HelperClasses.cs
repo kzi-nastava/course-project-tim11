9f87b43ba5211981fbe086f5fefe9e5e1799cf9d
stefanium1389 <stefanbogdjr@gmail.com>
2022-05-11 15:33:33 +0200

    Added DateRange class
    A small helper class to deal with anything that has a duration such as Appointments or Renovations

---
+++ ClinicApp/ClinicApp/HelperClasses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicApp
+{
+    public class DateRange
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public bool IsInRange(DateTime date)
+        {
+            return (date >= StartDate && date <= EndDate);
+        }
+        public bool IsOverlaping(DateRange other)
+        {
+            if (other.StartDate < StartDate && other.EndDate > StartDate)
+                return true;
+            if (other.StartDate > StartDate && other.StartDate < EndDate)
+                return true;
+            return false;
+        }
+        public void ValidateDates()
+        {
+            if (StartDate > EndDate)
+            {
+                DateTime x = StartDate;
+                StartDate = EndDate;
+                EndDate = x;
+            }
+        }
+    }
+}
