8c873dac9f307df78f42d8d4a7465352fe6032e3
stefanium1389 <stefanbogdjr@gmail.com>
2022-06-04 00:03:07 +0200

    Revert "Added operations"
    This reverts commit 47ea80c980b7688050012400038eba8d2ca34d78.

--- ClinicApp/ClinicApp/SystemFunctions.cs
+++ ClinicApp/ClinicApp/SystemFunctions.cs
@@ -32,8 +32,8 @@ namespace ClinicApp
         public static Dictionary<string, Doctor> Doctors { get; set; } = new Dictionary<string, Doctor>();
         public static Dictionary<string, Patient> Patients { get; set; } = new Dictionary<string, Patient>();
         public static Dictionary<string, HealthRecord> HealthRecords { get; set; } = new Dictionary<string, HealthRecord>();
-        public static Dictionary<int, Appointment> AllAppointments { get; set; } = new Dictionary<int, Appointment>();
-        public static Dictionary<int, Appointment> CurrentAppointments { get; set; } = new Dictionary<int, Appointment>();
+        public static Dictionary<int, Examination> AllExamtinations { get; set; } = new Dictionary<int, Examination>();
+        public static Dictionary<int, Examination> CurrentExamtinations { get; set; } = new Dictionary<int, Examination>();
 
         public static Dictionary<string, Medicine> Medicine { get; set; } = new Dictionary<string, Medicine>();
         public static List<Referral> Referrals { get; set; } = new List<Referral>();
@@ -41,7 +41,7 @@ namespace ClinicApp
         // User file path may change in release mode, this is the file path in debug mode
 
         public static string UsersFilePath = "../../../Data/users.txt";
-        public static string AppointmentsFilePath = "../../../Data/appointments.txt";
+        public static string ExaminationsFilePath = "../../../Data/examinations.txt";
         public static string HealthRecordsFilePath = "../../../Data/health_records.txt";
         public static string PatientRequestsFilePath = "../../../Data/patient_requests.txt";
         public static string ReferralsFilePath = "../../../Data/referrals.txt";
@@ -129,44 +129,42 @@ namespace ClinicApp
 
             //Loads the examinations.
 
-            using (StreamReader reader = new StreamReader(AppointmentsFilePath))
+            using (StreamReader reader = new StreamReader(ExaminationsFilePath))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
-
-                    Appointment appointment = ParseAppointment(line); 
-                    if (!AllAppointments.TryAdd(appointment.ID, appointment))
+                    Examination examination = new Examination(line);
+                    if (!AllExamtinations.TryAdd(examination.ID, examination))
                     {
-                        AllAppointments[appointment.ID] = appointment;
+                        AllExamtinations[examination.ID] = examination;
                     }
-                    if (appointment.DateTime.AddMinutes(15) > DateTime.Now)
+                    if (examination.DateTime.AddMinutes(15) > DateTime.Now)
                     {
-                        if (appointment.Tombstone != 0)
+                        if (examination.Tombstone != 0)
                         {
-                            CurrentAppointments.Remove(appointment.Tombstone);
+                            CurrentExamtinations.Remove(examination.Tombstone);
                         }
-                        else if (appointment.Edited != 0)
+                        else if (examination.Edited != 0)
                         {
-                            CurrentAppointments.Remove(appointment.Edited);
-                           if (!appointment.Finished)
-
+                            CurrentExamtinations.Remove(examination.Edited);
+                           if (!examination.Finished)
                             {
-                                CurrentAppointments.Add(appointment.ID, appointment);
+                                CurrentExamtinations.Add(examination.ID, examination);
                             }
                             
                         }
-                        else if (!appointment.Finished) {
-                            CurrentAppointments.Add(appointment.ID, appointment);
+                        else if (!examination.Finished) {
+                            CurrentExamtinations.Add(examination.ID, examination);
                         }
                     }
                     
                 }
-                foreach (int ID in CurrentAppointments.Keys)
+                foreach (int ID in CurrentExamtinations.Keys)
                 {
-                    Appointment currentAppointment = CurrentAppointments[ID];
-                    currentAppointment.Doctor.Appointments.Add(currentAppointment);
-                    currentAppointment.Patient.Appointments.Add(currentAppointment);
+                    Examination currentExamination = CurrentExamtinations[ID];
+                    currentExamination.Doctor.Examinations.Add(currentExamination);
+                    currentExamination.Patient.Examinations.Add(currentExamination);
                 }
             }
             //Loads the messages.
@@ -199,17 +197,6 @@ namespace ClinicApp
             return new Nobody();
         }
 
-        private static Appointment ParseAppointment(string line) {
-            string[] data = line.Split('|');
-            if (data[8] == "o")
-            {
-                return new Operation(line);
-            }
-            else {
-                return new Examination(line);
-            }
-        }
-
         // Uploads the information from the objects into the database
         public static void UploadData()
         {
@@ -225,9 +212,9 @@ namespace ClinicApp
                 }
             }
             //Uploads the examinations.
-            using (StreamWriter sw = File.CreateText(AppointmentsFilePath))
+            using (StreamWriter sw = File.CreateText(ExaminationsFilePath))
             {
-                foreach (KeyValuePair<int, Appointment> pair in AllAppointments)
+                foreach (KeyValuePair<int, Examination> pair in AllExamtinations)
                 {
                     newLine = pair.Value.Compress();
                     sw.WriteLine(newLine);
@@ -237,6 +224,15 @@ namespace ClinicApp
             using (StreamWriter sw = File.CreateText(HealthRecordsFilePath))
             {
                 foreach (KeyValuePair<string, HealthRecord> pair in HealthRecords)
+                {
+                    newLine = pair.Value.Compress();
+                    sw.WriteLine(newLine);
+                }
+            }
+            //Uploads the examinations.
+            using (StreamWriter sw = File.CreateText(ExaminationsFilePath))
+            {
+                foreach(KeyValuePair<int, Examination> pair in AllExamtinations)
                 {
                     newLine = pair.Value.Compress();
                     sw.WriteLine(newLine);
