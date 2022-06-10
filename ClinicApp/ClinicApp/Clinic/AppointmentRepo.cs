1dd08536c19e9f761239d125cca7aa56f7717c8a
stefanium1389 <stefanbogdjr@gmail.com>
2022-06-10 14:42:55 +0200

    fixed appointment repo not to crash the program

--- ClinicApp/ClinicApp/Clinic/AppointmentRepo.cs
+++ ClinicApp/ClinicApp/Clinic/AppointmentRepo.cs
@@ -12,10 +12,6 @@ namespace ClinicApp.Clinic
 
         public static string AppointmentsFilePath = "../../../Data/appointments.txt";
 
-        static AppointmentRepo()
-        {
-            LoadAppointments();
-        }
         public static Dictionary<int, Appointment> GetAll() => AllAppointments;
 
         public static void Add(Appointment appointment)
