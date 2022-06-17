7bdfdbd937aac08dec68d8ce36c8008b4e8dcebb
stefanium1389 <stefanbogdjr@gmail.com>
2022-06-17 11:24:39 +0200

    ClinicSurvey and DoctorSurvey bugifx

---
+++ ClinicApp/ClinicApp/Clinic/DoctorSurveyRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ClinicApp.Clinic
+{
+    public class DoctorSurveyRepo
+    {
+        public static List<DoctorSurvey> DoctorSurveys { get; set; }
+        public static string FilePathDoctorSurveys = "../../../Data/doctorsurveys.txt";
+        public DoctorSurveyRepo()
+        {
+        }
+
+        public static void LoadDoctorSurveys()
+        {
+            using (StreamReader reader = new StreamReader(FilePathDoctorSurveys))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    DoctorSurvey survey = new DoctorSurvey(line);
+                    AddDoctorSurvey(survey);
+                }
+            }
+        }
+
+
+        public static void AddDoctorSurvey(DoctorSurvey survey)
+        {
+            DoctorSurveys.Add(survey);
+            PersistChanges();
+        }
+
+        public static void PersistChanges()
+        {
+            string newLine;
+            using (StreamWriter sw = File.CreateText(FilePathDoctorSurveys))
+            {
+                foreach (DoctorSurvey survey in DoctorSurveys)
+                {
+                    newLine = survey.Compress();
+                    sw.WriteLine(newLine);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ClinicApp/ClinicApp/Clinic/DoctorSurveyService.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClinicApp.Users;
+
+namespace ClinicApp.Clinic
+{
+    public class DoctorSurveyService
+    {
+        public DoctorSurveyService()
+        {
+        }
+
+        public static void RateDoctor(Doctor doctor)
+        {
+            bool recommend = false;
+            Console.WriteLine("Please rate doctor from 0 to 5:");
+            int doctorRate = CLI.CLIEnterNumberWithLimit(0,5);
+            Console.WriteLine("Would you like to recommend this doctor to your friend?(yes/no)");
+            string userInput = Console.ReadLine();
+            if (userInput.ToLower() == "yes")
+            {
+                recommend = true;
+            }
+            Console.WriteLine("Leave a comment if you want, it's optional:");
+            string comment = Console.ReadLine();
+            DoctorSurveyRepo.AddDoctorSurvey(new DoctorSurvey(doctor,doctorRate,recommend,comment)); ;
+        }
+
+        public static Dictionary<Doctor, int> GetDoctorAverages()
+        {
+            Dictionary<Doctor, List<int>> ratings = new Dictionary<Doctor, List<int>>();
+            Dictionary<Doctor, int> averages = new Dictionary<Doctor, int>();
+            foreach (var survey in DoctorSurveyRepo.DoctorSurveys)
+            {
+                ratings[survey.RatedDoctor].Add(survey.DoctorRating);
+            }
+            foreach (var key in ratings.Keys)
+            {
+                int average = 0;
+                int count = 0;
+                foreach (var rating in ratings[key])
+                {
+                    average += rating;
+                    count++;
+                }
+                average = average / count;
+                averages[key] = average;
+            }
+            return averages;
+        }
+
+        public static Dictionary<Doctor, List<string>> GetComments()
+        {
+            Dictionary<Doctor, List<string>> comments = new Dictionary<Doctor, List<string>>();
+            foreach (var survey in Clinic.DoctorSurveyRepo.DoctorSurveys)
+            {
+                comments[survey.RatedDoctor].Add(survey.CustomersComment);
+            }
+            return comments;
+        }
+
+        public static Dictionary<Doctor, List<int>> GetHstograms()
+        {
+            Dictionary<Doctor, List<int>> histogram = new Dictionary<Doctor, List<int>>();
+            foreach (var survey in Clinic.DoctorSurveyRepo.DoctorSurveys)
+            {
+                if (!histogram.ContainsKey(survey.RatedDoctor))
+                {
+                    histogram[survey.RatedDoctor].Add(0);
+                    histogram[survey.RatedDoctor].Add(0);
+                    histogram[survey.RatedDoctor].Add(0);
+                    histogram[survey.RatedDoctor].Add(0);
+                    histogram[survey.RatedDoctor].Add(0);
+                }
+            }
+            foreach (var survey in Clinic.DoctorSurveyRepo.DoctorSurveys)
+            {
+                switch (survey.DoctorRating)
+                {
+                    case 1:
+                        histogram[survey.RatedDoctor][0]++;
+                        break;
+                    case 2:
+                        histogram[survey.RatedDoctor][1]++;
+                        break;
+                    case 3:
+                        histogram[survey.RatedDoctor][2]++;
+                        break;
+                    case 4:
+                        histogram[survey.RatedDoctor][3]++;
+                        break;
+                    case 5:
+                        histogram[survey.RatedDoctor][4]++;
+                        break;
+                }
+            }
+            return histogram;
+        }
+
+        public static List<Doctor> SortedByAverageRating()
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            Dictionary<Doctor, int> averages = GetDoctorAverages();
+            var ordered = averages.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var doctor in ordered.Keys)
+                doctors.Add(doctor);
+            return doctors;
+        }
+
+    }
+}
